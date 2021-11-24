c5606d88a3581f1b8f96a7b5be3637bde74e6a42
marlon20161188f <mavilat@uni.pe>
2021-11-24 18:19:04 -0500

    Update constantes.php

--- Admin/config/constantes.php
+++ Admin/config/constantes.php
@@ -1,6 +1,6 @@
 <?php
-    //$url_site = 'https://admin-pres.herokuapp.com/';
-    $url_site = 'http://localhost/';
+    $url_site = 'https://admin-pres.herokuapp.com/';
+    //$url_site = 'http://localhost/';
     //$url_site = 'http://Adminapp.me/';
     //$url_site = 'https://admin-vehiculo.herokuapp.com/';
 ?>
