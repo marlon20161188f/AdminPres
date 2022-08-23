42796e2c4b5ddda4022d93c7a0ee08a970a6fde3
Manuel Figueroa <manuel.f27@hotmail.com>
2022-08-23 10:13:57 -0500

    log

--- Admin/config/constantes.php
+++ Admin/config/constantes.php
@@ -1,5 +1,6 @@
 <?php
-    $url_site = 'https://admin-pres.herokuapp.com/';
+    //$url_site = 'https://admin-pres.herokuapp.com/';
+    $url_site = 'https://prestamos-camasi.herokuapp.com/';
     //$url_site = 'http://localhost/';
     //$url_site = 'http://Adminapp.me/';
     //$url_site = 'https://admin-vehiculo.herokuapp.com/';
